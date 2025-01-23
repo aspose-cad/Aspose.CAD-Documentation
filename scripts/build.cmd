96f57947c2bc917a4df0017be8e70291216f6e39
Denis Demenko <denis.demenko@aspose.com>
2025-01-24 00:16:57 +0800

    rewind

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,2 +1,7 @@
+set ORIGINAL_DIR=%cd%
+set SCRIPT_DIR=%~dp0
+
+cd %SCRIPT_DIR%
 cmd /c "cd /d .. && npm install"
 "../node_modules/hugo-extended/vendor/hugo" --minify --source ../ --destination dist --cleanDestinationDir --logLevel debug --panicOnWarning --config lutsk-aspose-prototype/config.toml,lutsk-aspose-prototype/configs/docs.aspose.com-cad.toml --contentDir content
+cd %ORIGINAL_DIR%
